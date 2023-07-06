5828c2528cf69f37dee01e1b2e5e0ca201e7bf56
mitul000 <50392591+mitul000@users.noreply.github.com>
2023-07-06 13:44:28 -0400

    ec2 instance deployment with terraform
    initial commit

---
+++ terraform_code/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Mitul"
+    "App"   = "Application2"
+    "Project" = "CLO835"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "week11"
+}
